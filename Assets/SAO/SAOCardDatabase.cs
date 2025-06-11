4f2a4f7b86e2b8713762686e9a8ce0c9d1242504
briankunkel9 <brainkunkel9@gmail.com>
2025-06-11 16:51:50 +0900

    스크립터블오브젝트 작성

---
+++ Assets/SAO/SAOCardDatabase.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SAOCardDatabase", menuName = "ScriptableObjects/Card Database")]
+public class SAOCardDatabase : ScriptableObject
+{
+    public enum Type
+    {
+        Knight,
+        Viking,
+        Western
+    }
+
+    [System.Serializable]
+    public class UnitElement
+    {
+        public GameObject Unit;
+        public string UnitName;
+        public int UnitAmount;
+    }
+    
+    
+    [System.Serializable]
+    public class CardData
+    {
+        public string ID;
+        public string Name;
+        public int Rank;
+        public Type Type;
+        public List<UnitElement> Units;
+        public string Description;
+        public Sprite Image;
+        public bool Triple = false; // 트리플 카드 여부
+    }
+    
+    public List<CardData> cards = new List<CardData>();
+    
+    public CardData GetCardById(string id)
+    {
+        foreach (CardData entry in cards)
+        {
+            if (entry.ID == id)
+            {
+                return entry;
+            }
+        }
+        Debug.LogWarning($"Sprite with ID '{id}' not found in {name}.");
+        return null;
+    }
+}
