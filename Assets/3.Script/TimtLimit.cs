31a5a1ecda0d2fc98255773b8352cd49850ce176
briankunkel9 <brainkunkel9@gmail.com>
2025-06-16 10:11:13 +0900

    타이머 기능 구현

---
+++ Assets/3.Script/TimtLimit.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TimtLimit : MonoBehaviour
+{
+    private ReadyTurn readyTurn;
+    private TMP_Text timeText;
+    public float inputTimeLimitSecond = 30f;
+    private float tempTimeLimitSecond;
+    private int minute;
+    private int second;
+
+    private void Start()
+    {
+        transform.root.TryGetComponent(out readyTurn);
+        transform.TryGetComponent(out timeText);
+        tempTimeLimitSecond = inputTimeLimitSecond;
+        SecondToMinute();
+    }
+
+    private void Update()
+    {
+        SecondToMinute();
+        UpdateTimer();
+        FlowTime();
+    }
+
+    private void UpdateTimer()
+    {
+        if (minute >= 0 || second >= 0)
+        {
+            timeText.text = minute > 9 ? $"{minute}:" : $"0{minute}:" +
+                            (second > 9 ? $"{second}" : $"0{second}");
+        }
+    }
+
+    private void FlowTime()
+    {
+        tempTimeLimitSecond -= Time.deltaTime;
+    }
+    
+    private void SecondToMinute()
+    {
+        minute = Mathf.FloorToInt(tempTimeLimitSecond / 60);
+        second = Mathf.FloorToInt(tempTimeLimitSecond % 60);
+    }
+}
